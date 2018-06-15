5dd769e8a582ba23a14e0bfc67fc503fe623eb05
Benjamin W Wulf <benjaminwulf@gmail.com>
2018-06-14 18:29:41 -0600

    got attackers and defenders

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,25 +7,23 @@ var attackerHp = "";
 var defenderHp = "";
 var isAttacker = false;
 
-// Create object with all character cards
-var characters = [
-        {
-        name: "Henrike Warsaw",
-        hp: 140
-        },
-        {
-        name: "Hansel Heidelber",
-        hp: 112
-        },
-        {
-        name: "Race Car",
-        hp: 88
-        },
-        {
-        name: "Ruby",
-        hp: 120
-        }
-    ]
+// Create objects with all character cards
+var hw = {
+        "name": "Henrike Warsaw",
+        "hp": 140
+        };
+var hh = {
+        "name": "Hansel Heidelber",
+        "hp": 112
+        };
+var rc = {
+        "name": "Race Car",
+        "hp": 88
+        };
+var rb = {
+        "name": "Ruby",
+        "hp": 120
+        };
 
 // Use jQuery to display hp to DOM
 $("#hp-hw").text("HP: " + hpHw);
@@ -34,90 +32,65 @@ $("#hp-rc").text("HP: " + hpRc);
 $("#hp-rb").text("HP: " + hpRb);
 
 // Move card to fight section on click of attack-button
+// If "#choose-enemy-button" not clicked, isAttacker set to false
 // If "#choose-enemy-button" clicked is true, run defender function
-// If "#choose-enemy-button" not clicked, run attacker function
-if (isAttacker == true) {
-    $("#choose-enemy-button").on("click", function defender() {
-        $("#card-hw.card").on("click", function() {
-            defenderHp = hpHw;
-            console.log(defenderHp);
-            $("#card-hw").appendTo($("#defender"));
-        });
-        $("#card-hh.card").on("click", function() {
-            defenderHp = hpHh;
-            console.log(defenderHp);
-            $("#card-hh").appendTo($("#defender"));
-        });
-        $("#card-rc.card").on("click", function() {
-            defenderHp = hpRc;
-            console.log(defenderHp);
-            $("#card-rc").appendTo($("#defenderr"));
-        });
-        $("#card-rb.card").on("click", function() {
-            defenderHp = hpRb;
-            console.log(defenderHp);
-            $("#card-rb").appendTo($("#defender"));
-        });
-    });
-} else if (isAttacker == false) { 
+$("#choose-enemy-button").on("click", function() {
+    isAttacker = true;
+    console.log(isAttacker);
+ });
+
+// On click of Henrika Warsaw card, run this function
 $("#card-hw.card").on("click", function() {
-    isAttacker = true;
-    attackerName = characters.name;
-    attackerHp = hpHw;
-    console.log(attackerHp);
-    $("#card-hw").appendTo($("#selected-character"));
+    if (isAttacker) {
+        defenderHp = hpHw;
+        console.log(defenderHp);
+        $("#card-hw").appendTo($("#defender"));
+    }
+    else {
+        attackerHp = hpHw;
+        console.log(attackerHp);
+        $("#card-hw").appendTo($("#selected-character"));
+    }
 });
+
+// On click of Hansel Heidelberg card, run this function
 $("#card-hh.card").on("click", function() {
-    isAttacker = true;
-    attackerName = characters.name;
-    attackerHp = hpHh;
-    console.log(attackerHp);
-    $("#card-hh").appendTo($("#selected-character"));
+    if (isAttacker) {
+        defenderHp = hpHh;
+        console.log(defenderHp);
+        $("#card-hh").appendTo($("#defender"));
+    }
+    else {
+        attackerHp = hpHh;
+        console.log(attackerHp);
+        $("#card-hh").appendTo($("#selected-character"));
+    }
 });
+
+// On click of Race Car card, run this function
 $("#card-rc.card").on("click", function() {
-    isAttacker = true;
-    attackerName = characters.name;
-    attackerHp = hpRc;
-    console.log(attackerHp);
-    $("#card-rc").appendTo($("#selected-character"));
+    if (isAttacker) {
+        defenderHp = hpRc;
+        console.log(defenderHp);
+        $("#card-rc").appendTo($("#defender"));
+    }
+    else {
+        attackerHp = hpRc;
+        console.log(attackerHp);
+        $("#card-rc").appendTo($("#selected-character"));
+    }
 });
+
+// On click of Ruby card, run this function
 $("#card-rb.card").on("click", function() {
-    isAttacker = true;
-    attackerName = characters.name;
-    attackerHp = hpRb;
-    console.log(attackerHp);
-    $("#card-rb").appendTo($("#selected-character"));
+    if (isAttacker) {
+        defenderHp = hpRb;
+        console.log(defenderHp);
+        $("#card-rb").appendTo($("#defender"));
+    }
+    else {
+        attackerHp = hpRb;
+        console.log(attackerHp);
+        $("#card-Rb").appendTo($("#selected-character"));
+    }
 });
-} 
-// On click "id="attack-button" the immediate following click of card will move id="defender"
-// $("#choose-enemy-button").on("click", function defender() {
-//     $("#card-hw.card").on("click", function() {
-//         defenderHp = hpHw;
-//         console.log(defenderHp);
-//         $("#card-hw").appendTo($("#defender"));
-//     });
-//     $("#card-hh.card").on("click", function() {
-//         defenderHp = hpHh;
-//         console.log(defenderHp);
-//         $("#card-hh").appendTo($("#defender"));
-//     });
-//     $("#card-rc.card").on("click", function() {
-//         defenderHp = hpRc;
-//         console.log(defenderHp);
-//         $("#card-rc").appendTo($("#defenderr"));
-//     });
-//     $("#card-rb.card").on("click", function() {
-//         defenderHp = hpRb;
-//         console.log(defenderHp);
-//         $("#card-rb").appendTo($("#defender"));
-//     });
-// });
-    // $("#choose-enemy-message").text("Choose a character to attack!");
-    // $(".card").on("click", function() {
-    //      defender = this.key.hp;
-    //      $(this).appendTo($("#defender"));
-    // });
-
-
-// Here we are creating logic to adjust HP based on Math.random in attack
-
